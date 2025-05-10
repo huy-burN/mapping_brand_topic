6025d942cd9688ce5dd49a3d017c2314bfde5a63
huy-burN <nhathuy200712@gmail.com>
2025-05-10 18:52:48 +0700

    lap2

--- src/run.py
+++ src/run.py
@@ -34,7 +34,7 @@ def main(input_excel):
     excel_path = "processed_data.xlsx"
     raw_data_df.to_excel(excel_path, index=False)
     
-    # Trích xuất thông tinnn
+    # Trích xuất thông tinnnn
     for _, row in info_df.iterrows():
         gen_mapping(row['object'], row['field'], row['definition'])
     
