0b408e4b79ad474faf611f88c112811480169618
huy-burN <nhathuy200712@gmail.com>
2025-09-26 23:49:54 +1000

    bh t logi ua

--- clean_text.py
+++ clean_text.py
@@ -13,7 +13,7 @@ def clean_text(text):
     text = text.lower().strip()
     return text
 
-# Đường dẫn file và tên sheet cần đọc
+# Đường dẫn file và tên sheet cần đọcc
 input_file = "C:/Users/admin/Downloads/test_work.xlsx"
 sheet_test = "test"
 
